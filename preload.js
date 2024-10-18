aa83617a0f071710cc503775047b0638e2bac0fe
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-10-18 22:08:25 +0700

    Update

--- preload.js
+++ preload.js
@@ -1,11 +1,24 @@
-const { exec } = require("child_process");
-const path = require("path");
-const fs = require("fs");
 const { contextBridge, ipcRenderer } = require("electron");
 
+// Cung cấp các chức năng điều khiển cửa sổ (minimize, maximize, close)
 contextBridge.exposeInMainWorld("windowControls", {
   minimize: () => ipcRenderer.send("minimizeApp"),
   maximize: () => ipcRenderer.send("maximizeApp"),
   close: () => ipcRenderer.send("closeApp"),
 });
 
+// Cung cấp các API cho thao tác với video (fetch video info, download video)
+contextBridge.exposeInMainWorld("api", {
+  getVideoInfo: (url) => ipcRenderer.invoke("fetch-video-info", url),
+
+  // Gửi yêu cầu tải video với các tham số URL, format, thời gian bắt đầu, thời gian kết thúc, và chất lượng
+  downloadVideo: (url, format, startTime, endTime) => {
+    return ipcRenderer.invoke(
+      "download-video",
+      url,
+      format,
+      startTime,
+      endTime
+    );
+  },
+});
